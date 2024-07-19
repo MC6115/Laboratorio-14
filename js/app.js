f03d71fd29577be062a980813fc94c97bc24e007
VERONICAHR27 <hilarioreyesveronica6@gmail.com>
2024-07-18 21:26:45 -0500

    Corrigiendo errores

--- js/app.js
+++ js/app.js
@@ -19,16 +19,23 @@ AppState.prototype.instantiateProducts = function () {
 }
 
 AppState.prototype.saveToLocalStorage = function () {
-  for(let i=0;i<this.allProducts.length;i++){
-    localStorage.setItem(`${this.allProducts[i].name}`, JSON.stringify(this.allProducts[i]));
-    console.log(JSON.parse(localStorage.getItem(`${this.allProducts[i].name}`)))
-  }
+    localStorage.setItem(`datos`, JSON.stringify(this.allProducts));
+   
+  
 }
 
 AppState.prototype.loadItems = function () {
-
-  // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
-
+  // TODO: Update this instance method to retrieve data from local storage instead of creating new 
+  // Products on each page load
+ // if (localStorage.getItem(`${bag}`)) {
+  //  for (let i = 0; i < productNames.length; i++) {
+   //   const acceder = JSON.parse(localStorage.getItem(`${this.allProducts[i].name}`))
+   //   this.allProducts.push(acceder)
+  //  }
+  //}
+  //else{
+   // this.instantiateProducts();
+  //} 
   this.instantiateProducts();
 }
 
